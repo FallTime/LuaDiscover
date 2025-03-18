880e404ab97e9e7b851ac4b10716e87ac7d9ff0c
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2025-03-18 18:42:09 -0300

    code - exercise 1.8 solved

---
+++ Codes/Programming in Lua/Chapter1/exercise1.8.lua
@@ -0,0 +1,6 @@
+--[[
+Write a simple script that prints its own name without knowing it in advance.
+]]--
+
+scriptName = arg[0] -- Retrieves the script's filename from the command-line arguments
+print("The script's name is: " .. scriptName)
