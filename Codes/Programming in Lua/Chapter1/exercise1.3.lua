33fa216f2cc031a41484e1237523e8f1ff136d1b
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2025-03-18 17:41:27 -0300

    fix - empty script

---
+++ Codes/Programming in Lua/Chapter1/exercise1.3.lua
@@ -0,0 +1,5 @@
+--[[
+Can you name other languages that use "--" for comments?
+R: The only one I know besides lua that comments with "--" is SQL. 
+In some searches, I found out about Haskell and Ada, other languages that they also use.
+]]--
