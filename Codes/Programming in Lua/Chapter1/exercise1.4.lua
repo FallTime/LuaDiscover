941ff9759a84ed3f2e661d81e559ebfb044d3a2d
Felipe França <felipe.22204708360072@faeterj-rio.edu.br>
2025-03-18 18:02:31 -0300

    code - exercise 1.4 solved

---
+++ Codes/Programming in Lua/Chapter1/exercise1.4.lua
@@ -0,0 +1,15 @@
+--[[
+Which of the following strings are valid identifiers?
+  ___ it's valid.
+  _end it's valid.
+  End it's valid.
+  end not valid, is a reserved word.
+  until? not valid, until is a reserved word but problem here is the question mark.
+  nil not valid, is a reserved word.
+  NULL it's valid.
+  one-step not valid, arithmetic operator in name.
+
+]]---
+
+___, _end, End = 10, 10, 10
+print(___, _end, End)
