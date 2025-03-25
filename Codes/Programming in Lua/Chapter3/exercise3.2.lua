91288d31ddbf70589406c5dc4de5ce9d6563aaf4
Felipe França <111690931+FallTime@users.noreply.github.com>
2025-03-25 15:58:29 -0300

    fix - exercise 3.2 Indentation

--- Codes/Programming in Lua/Chapter3/exercise3.2.lua
+++ Codes/Programming in Lua/Chapter3/exercise3.2.lua
@@ -15,25 +15,25 @@ Exercise 3.2: Explain the following results:
 
  > math.mininteger * 2 --> 0
 
- 	math.mininteger - math.maxinteger 		 --> 1
+ 	math.mininteger - math.maxinteger 	 --> 1
  	math.mininteger - (-1 + math.mininteger) --> 1
- 	math.mininteger * 2 + 1 				 --> 1
- 	math.mininteger * 2  			 		 --> 0
+ 	math.mininteger * 2 + 1 		 --> 1
+ 	math.mininteger * 2  			 --> 0
 
  > math.maxinteger * math.maxinteger --> 1
 
 	(1 + math.maxinteger) * (1 + math.maxinteger)  --> 0
 	1 + 2 * math.maxinteger + math.maxinteger ** 2 --> 0
-	1 - 2 + math.maxinteger ** 2 				   --> 0
-	-1 + math.maxinteger ** 2 				       --> 0
-	math.maxinteger ** 2						   --> 1
+	1 - 2 + math.maxinteger ** 2 		       --> 0
+	-1 + math.maxinteger ** 2 		       --> 0
+	math.maxinteger ** 2			       --> 1
 	
 
  > math.mininteger * math.mininteger --> 0
 
  	(-1 + math.mininteger) * (-1 + math.mininteger) --> 1
 	1 - 2 * math.mininteger + math.mininteger ** 2  --> 1
-	1 - 0 + math.mininteger ** 2 					--> 1
-	math.mininteger ** 2 							--> 0
+	1 - 0 + math.mininteger ** 2 			--> 1
+	math.mininteger ** 2 				--> 0
 
 ]]--
